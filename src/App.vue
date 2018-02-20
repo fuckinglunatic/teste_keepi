7e82c1d9737f43c738b65b5710b5482649f27603
Daniel Costa <daniel.costa@nambbu.com.br>
2018-02-20 17:20:20 -0300

    Atualização dos comentários no código

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <ul>
       <!-- For do vue para a propriedade passada -->
       <li v-for="cards in deck" :key="cards._id" :class="cards.rarity">
+        <!-- Passa as váriaveis do for para o card -->
         <img :src="'http://www.clashapi.xyz/images/cards/' + cards.idName + '.png'" :alt="cards.name" :title="cards.name">
         <div class="overlay">
           <h2>{{cards.name}}</h2>
@@ -13,6 +14,7 @@
       </li>
     </ul>
     <div class="footer">
+      <!-- Botão simples para chamar novamente o metodo de gerar um deck aleatório -->
       <button v-on:click="getDeck" class="action-button shadow animate red">Gerar deck novo</button>
     </div>
   </div>
@@ -20,6 +22,7 @@
 <script>
 export default {
   data () {
+    // Define as variáveis a serem usadas
     return {
       deck: [],
       decks: []
@@ -30,7 +33,9 @@ export default {
     this.getDeck()
   },
   methods: {
+    // Metodo para gerar o deck
     getDeck: function () {
+      // Faz a requisição na URL e gera uma premissa para tratar a resposta
       this.$http.get('http://www.clashapi.xyz/api/random-deck').then(function ({data}) {
         this.deck = data
         console.log(this.deck)
